146bf4a7f3e66f320b8c50202d9f29a4b2243712
Ayush negi <87976275+yuyuayush@users.noreply.github.com>
2024-05-23 22:09:56 +0530

    s

--- data/index.ts
+++ data/index.ts
@@ -10,7 +10,7 @@ export const navItems = [
       id: 1,
       title: "I prioritize client collaboration, fostering open communication ",
       description: "",
-      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      className: "lg:col-span-3 md:col-span-6 sm:col-span-6 sm:row-span-4 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
       titleClassName: "justify-end",
       img: "/b1.svg",
